07d2c7fb5a77d5151fceaa140aef519eabec22ae
Marta Masramon <marta.masramon@gmail.com>
2021-02-04 18:52:24 +0000

    Proxy in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-   /* other options*/
     target: 'web',
     node: {
         fs: 'empty'
+    },
+    devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://evening-eyrie-40887.herokuapp.com',
+        secure: false
+      }
     }
-/* ... */
+  }
 }
