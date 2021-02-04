6bacd48f09395744760b9692ecfdeea6ad5cac20
Marta Masramon <marta.masramon@gmail.com>
2021-02-04 17:35:37 +0000

    target backend

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,14 @@ module.exports = {
     target: 'web',
     node: {
         fs: 'empty'
-    }
+    },
+    devServer: {
+      proxy: {
+        '/api': {
+          target: 'https://evening-eyrie-40887.herokuapp.com/',
+          secure: false
+        }
+      }
+  }
 /* ... */
 }
